228954ba70f338bb75c82c57f536f188731e5c75
crusso <crusso@linus.cs.loyola.edu>
2017-12-12 21:41:15 -0500

    Edit it

--- src/ArrayIndexOutOfBounds_Test.java
+++ src/ArrayIndexOutOfBounds_Test.java
@@ -5,13 +5,13 @@ public class ArrayIndexOutOfBounds_Test
 {
 	public static void main(String[] args) {
 		int [] testArray = new int[2];
-		fifthElement(testArray);
+		Element(testArray);
 	}
 	
-	public static int fifthElement(int [] testing) {
+	public static int Element(int [] testing) {
 		int num = 0; 
-		for(int i = 0; i <= 5; i++) {
-		    num = testing[4];
+		for(int i = 0; i <= testing.length; i++) {
+		    num = testing[i];
 		}
 		return num;
 	}
